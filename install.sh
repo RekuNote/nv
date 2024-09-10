cd32b24ecadd763b84f42f9d8880987a31035d18
RekuNote <rekunote@gmail.com>
2024-09-10 14:46:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ function install_nv {
 
     echo -e "${INFO_COLOR}nv installed successfully! 🎉${RESET_COLOR}"
     sudo nv update
-    echo -e "\n\033[35mWelcome to nv package manager, \033[32m$(whoami)\033[35m.\033[0m\n"
+    echo -e "\n\033[35mWelcome to nv package manager, \033[32m$(whoami)\033[35m.\033[0m"
     nv
 }
 
